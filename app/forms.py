43a0de1ce447bab07f0a0540e1dac0c9702abadf
Lucas <lucasribeiroprogramador@gmail.com>
2022-04-26 07:25:18 -0300

    first commit

---
+++ app/forms.py
@@ -0,0 +1,7 @@
+from django.forms import ModelForm
+from app.models import Pessoas
+
+class PessoasForm(ModelForm):
+    class Meta:
+        model = Pessoas
+        fields = ['nome', 'profissão', 'idade']
